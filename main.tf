4239a24b4a66ba64d776ad0543bd832246803099
teekay2020 <tokunbo_kazeem@hotmail.co.uk>
2019-08-19 10:47:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,35 @@
-resource "aws_elastic_beanstalk_application" "test" {
-  name        = "ebs-test2"
+
+resource "aws_elastic_beanstalk_application" "myapp" {
+  name        = "Tutorial_App"
   description = "Test of beanstalk deployment"
 }
 
-resource "aws_elastic_beanstalk_environment" "test_env" {
-  name                = "ebs-test-env"
-  application         = "${aws_elastic_beanstalk_application.test.name}"
-  cname_prefix        = "100034mytest"
+resource "aws_elastic_beanstalk_environment" "tkbest_green" {
+  name                = "Tutorial-App-env1-green"
+  application         = "${aws_elastic_beanstalk_application.myapp.name}"
+  cname_prefix        = "mytestprojectapp1green"
 
  solution_stack_name = "64bit Amazon Linux 2017.03 v2.6.5 running Tomcat 8 Java 8"
- version_label = "latest"
 
- depends_on = ["aws_elastic_beanstalk_application_version.latest"]
 }
 
-resource "aws_elastic_beanstalk_application_version" "latest" {
-  name        = "latest"
-  application = "${aws_elastic_beanstalk_application.test.name}"
-  description = "Version latest of app ${aws_elastic_beanstalk_application.test.name}"
-  bucket      = "elasticbeanstalk-eu-west-2-877757016492"
-  key         = "samples100.war"
-  #bucket      = "elasticbeanstalk-eu-west-1-877757016492"
-  #key         = "20173035LD-samples.war"
-}
+resource "aws_elastic_beanstalk_environment" "tkbest_blue" {
+  name                = "Tutorial-App-env1-blue"
+  application         = "${aws_elastic_beanstalk_application.myapp.name}"
+  cname_prefix        = "mytestprojectapp1blue"
 
+ solution_stack_name = "64bit Amazon Linux 2017.03 v2.6.5 running Tomcat 8 Java 8"
+
+}
 
 provider aws {
   region = "eu-west-1"
 }
+
+output "cname-green" {
+  value = "${aws_elastic_beanstalk_environment.tktest_green.cname}"
+}
+
+
+output "cname-blue" {
+  value = "${aws_elastic_beanstalk_environment.tktest_blue.cname}"
